eb25b36869e1828c64a2af46238b939eee4ea5ee
Sergey Shcherbina <trytoheal@gmail.com>
2019-03-14 23:19:40 +0500

    remove unused fields

--- amqp_2phase/models.py
+++ amqp_2phase/models.py
@@ -1,30 +1,14 @@
 from django.db import models
 
-EVENT_STATUS_PENDING = 1
-EVENT_STATUS_COMPLETE = 2
-
-EVENT_STATUS_CHOICES = (
-    (EVENT_STATUS_PENDING, 'Pending'),
-    (EVENT_STATUS_COMPLETE, 'Complete'),
-)
-
 
 class AMQPEvent(models.Model):
-    create_date = models.DateTimeField(auto_now_add=True, null=False, blank=False)
-    send_date = models.DateTimeField(null=True, blank=True)
-    status = models.SmallIntegerField(choices=EVENT_STATUS_CHOICES, null=False, blank=False,
-                                      default=EVENT_STATUS_PENDING)
     exchange = models.CharField(max_length=32, null=False, blank=False)
-    event_type = models.CharField(max_length=32, null=False, blank=False, db_column='type')
     data = models.TextField(null=False, blank=True)
 
     def __str__(self):
         return str(self.id)
 
     def save(self, *args, **kwargs):
-        if not self.event_type:
-            raise ValueError('{}.event_type cannot be blank.'.format(self.__class__.__name__))
-
         if not self.exchange:
             raise ValueError('{}.exchange cannot be blank.'.format(self.__class__.__name__))
 
